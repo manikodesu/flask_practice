a8ec71952910876cb185430ee94e00311894015e
maniko <241164343+manikodesu@users.noreply.github.com>
2025-11-11 17:23:32 +0900

    feat: add dynamic route / hello/<name> - fix push - 2025/11/11

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
+
 def hello_world():
-    return '<p>Hello World!<P>'
+    return "<h1>Hello world!<h1>"
+
+@app.route('/hello/<name>')
+def grret(name):
+    return f"<h1>Hello {name}!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
